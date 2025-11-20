e76d3cfc9d20741b274d33776810e385f02c8069
koushik-2708 <koushikmunnangi2008@gmail.com>
2025-11-20 21:22:18 +0530

    Add files via upload

---
+++ identify duplicate elements from array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n;
+    printf("Enter size of array: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid size.\n");
+        return 1;
+    }
+    int arr[n];
+    printf("Enter any %d elements in array: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Duplicate Elements are: ");
+    for (i = 0; i < n; i++) {
+        int rep = 0;
+        int cnt = 0;
+        for (j = 0; j < n; j++) {
+            if (j < i && arr[i] == arr[j]) {
+                rep = 1;
+                break;
+            }
+            if (arr[i] == arr[j]) {
+                cnt++;
+            }
+        }
+        if (!rep && cnt > 1) {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
